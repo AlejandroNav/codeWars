0ba4fab70c04a848f85a95ccf3966f145add17b1
Alex-Nav <alejugger@gmail.com>
2023-02-21 10:50:01 -0600

    final changes

---
+++ 8kyu/xo.js
@@ -0,0 +1,8 @@
+//Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive.
+// The string can contain any char.
+function XO(str) {
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return resul; //no need to compare
+}
+console.log(XO("HOLA QUE HACE"));
